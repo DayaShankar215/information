5e4105b48e038ae41a03268d9a0199e0e12a5705
Daya Shankar Adhikari <dayashankaradhikari@gmail.com>
2025-04-16 19:23:35 +0545

    updated

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import { jsx as _jsx } from "react/jsx-runtime";
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(/*#__PURE__*/_jsx(React.StrictMode, {
+  children: /*#__PURE__*/_jsx(App, {})
+}));
