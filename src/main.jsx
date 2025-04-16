9c45653299ec20c637bcb892ca7b5a3061a13000
Daya Shankar Adhikari <dayashankaradhikari@gmail.com>
2025-04-16 13:36:40 +0545

    newly updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = "256432281690-33u8ba99v0u1hr5j56knh8f3vi2huc4j.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
